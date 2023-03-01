fc605ceeaa5312fe25ab2933c575cb2b70f28ebe
DiegoCh5478 <dmorales-2021420@kinal.edu.gt>
2023-03-01 09:44:10 -0600

    Se agregoron los middlewares y helpers

---
+++ src/helpers/create-jwt.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secretyKey = process.env.SECRET_KEY;
+
+const generateJWT = async(id, companyName)=>{
+    const payLoad = {id, companyName};
+    try {
+        const token = await jwt.sign(payLoad, secretyKey, {
+            expiresIn: '1h'
+        });
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+module.exports = {generateJWT};
